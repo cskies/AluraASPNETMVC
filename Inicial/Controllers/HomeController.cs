63072ec4c3136ab8c851fa48c7bed6a6f8462f2f
cskies <cskies@hotmail.com>
2016-12-07 11:36:54 -0200

    1st

---
+++ Inicial/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inicial.Controllers
+{
+    //every controller inherits this clas Controller
+    public class HomeController : Controller
+    {
+        // GET: Home //action atende request web e sempre return ACtionResult
+        public ActionResult Index()
+        {
+            //chama a logica de negocio
+
+            //depois redireciona para a view
+            return View();
+        }
+    }
+}
